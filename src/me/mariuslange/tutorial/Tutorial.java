bf31d5a3a367c9c82109f9aa2077c72e136b98d6
DeBukkitTutorials <DeBukkitTutorials@googlemail.com>
2013-10-13 16:32:26 +0200

    Code aus Folge 2

---
+++ src/me/mariuslange/tutorial/Tutorial.java
@@ -0,0 +1,25 @@
+package me.mariuslange.tutorial;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tutorial extends JavaPlugin 
+{
+	@Override
+	public void onEnable()
+	{
+		System.out.println("Das Plugin wurde erfolgreich geladen!");
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
+		if(cmd.getName().equalsIgnoreCase("tutorial"))
+		{
+			sender.sendMessage("Du hast den Befehl /tutorial eingegeben!");
+		}
+		return true;
+	}
+	
+}
